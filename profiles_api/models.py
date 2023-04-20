b80dcf7c3e8bbccfc83a6aaf31b7d1d26f171f2e
Alex <aleleychen@gmail.com>
2023-04-20 17:23:40 -0600

    added perm

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from  django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
@@ -45,16 +46,28 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
  REQUIRED_FIELDS = ['name']
 
 
-def get_full_name(self):
- """"Retrieve full name us ouser"""
- return self.name    
+ def get_full_name(self):
+  """"Retrieve full name us ouser"""
+  return self.name    
 
 
-def get_short_name(self):
+ def get_short_name(self):
     """Retrieve short name of user"""
     return self.name    
 
 
-def __str__(self):
+ def __str__(self):
     """Return string representation of our user"""
     return self.email
+
+class ProfileFeedItem(models.Model):
+  """"profile status update"""
+  user_profile = models.ForeignKey(
+    settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE
+  )
+  status_text = models.CharField(max_length=255)
+  created_on = models.DateTimeField(auto_now_add=True)
+  def __str__(self):
+    return self.status_text
+    
